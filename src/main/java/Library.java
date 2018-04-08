f052eb3ec14b5f300af001bbf6de8098bee658ec
Effective Jenkins <effectivejenkins@gmail.com>
2018-04-08 12:01:00 +0200

    Testing webhooks

--- src/main/java/Library.java
+++ src/main/java/Library.java
@@ -5,6 +5,7 @@ public class Library {
     public boolean someLibraryMethod() {
         System.out.println("Hello World!");
         System.out.println("Hello World 2!");
+        System.out.println("Hello World 3!");
         return true;
     }
 }
