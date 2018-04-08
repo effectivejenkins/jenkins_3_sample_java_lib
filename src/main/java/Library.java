319b44e8e4a78f8499ea92475d48acbff4e255f7
Effective Jenkins <effectivejenkins@gmail.com>
2018-04-08 13:11:13 +0200

    Testing int & service 1

--- src/main/java/Library.java
+++ src/main/java/Library.java
@@ -7,6 +7,7 @@ public class Library {
         System.out.println("Hello World 2!");
         System.out.println("Hello World 3!");
         System.out.println("Hello World 4!");
+        System.out.println("Hello World 5!");
         return true;
     }
 }
